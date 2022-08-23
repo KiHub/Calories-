e05ad4c9304d7c6d5a82faffc9a2d69a0f4e4a96
Kirill Ki <kirillkalapov@gmail.com>
2022-08-23 18:02:42 +0200

    Initial Commit

---
+++ Calories+/Calories_App.swift
@@ -0,0 +1,17 @@
+//
+//  Calories_App.swift
+//  Calories+
+//
+//  Created by  Mr.Ki on 23.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Calories_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
